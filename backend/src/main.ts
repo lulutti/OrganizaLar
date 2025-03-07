5dbe7180fee4f2b86600cac8f48dea5dc257238c
Luiza Dragonetti <malludra@gmail.com>
2025-03-06 21:31:43 -0300

    [01.01] Adding migrations configuration on main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { connectionSource } from './config/typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  await connectionSource.initialize();
+
+  await connectionSource.runMigrations();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
